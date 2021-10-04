f71af08661754eee0c1828a01aa2e1980126b421
sharda suman <83698402+sharda2001@users.noreply.github.com>
2021-10-04 15:23:36 +0530

    Add files via upload

---
+++ emoji_question_.py
@@ -0,0 +1,18 @@
+import json
+from typing import List
+file1 = open("interview.json")
+intr= json.load(file1)
+list=[]
+list1=[]
+i=1
+for i in intr:
+   list.append(i["title"])
+   list1.append(i["symbol"])
+a=list
+b=list1
+dict={}
+res = {a[i]:b[i] for i in range(len(a))}
+user=input("enter the title")
+for i in res:
+    if user==i:
+        print(res[i])
